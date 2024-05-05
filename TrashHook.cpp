e48386874b068a74c6a3f0620f323c6fe1269457
Tervor <69775280+vpurana@users.noreply.github.com>
2024-05-05 10:57:13 +0800

    Add files via upload

--- TrashHook.cpp
+++ TrashHook.cpp
@@ -1,7 +1,6 @@
 #include "TrashHook.h"
 
 #include <gdiplus.h>
-#pragma comment (lib,"Gdiplus.lib")
 #include <MinHook.h>
 #include <gdiplustypes.h>
 #include <gdiplus.h>
@@ -13,7 +12,8 @@
 
 using namespace Gdiplus;
 
-#pragma comment(lib,"libMinHook.lib")
+#pragma comment(lib,"libMinHook-x86-v141-md.lib")
+#pragma comment (lib,"Gdiplus.lib")
 #pragma comment(lib,"Msimg32.lib")
 
 #define FOREGOUND_COLOR RGB(255, 0, 255)
@@ -22,124 +22,75 @@ using namespace Gdiplus;
 #define COLORREF2RGB(Color) (Color & 0xff00) | ((Color >> 16) & 0xff) \
                                  | ((Color << 16) & 0xff0000)
 
-//-------------------------------------------------------------------------------
-// ReplaceColor
-//
-// Author    : Dimitri Rochette drochette@coldcat.fr
-// Specials Thanks to Joe Woodbury for his comments and code corrections
-//
-// Includes  : Only <windows.h>
-
-//
-// hBmp         : Source Bitmap
-// cOldColor : Color to replace in hBmp
-// cNewColor : Color used for replacement
-// hBmpDC    : DC of hBmp ( default NULL ) could be NULL if hBmp is not selected
-//
-// Retcode   : HBITMAP of the modified bitmap or NULL for errors
-//
-//-------------------------------------------------------------------------------
+__declspec(noinline)
 HBITMAP ReplaceColor(HBITMAP hBmp, COLORREF cOldColor, COLORREF cNewColor, HDC hBmpDC, Rect* lpRect)
 {
+
 	HBITMAP RetBmp = NULL;
-	if (hBmp)
+	if (!hBmp)
 	{
-		HDC BufferDC = CreateCompatibleDC(NULL);    // DC for Source Bitmap
-		if (BufferDC)
+		return NULL;
+	}
+	HBITMAP hTmpBitmap = (HBITMAP)NULL;
+
+	HDC DirectDC = CreateCompatibleDC(NULL); // DC for working
+	if (DirectDC)
+	{
+		// Get bitmap size
+		BITMAP bm;
+		GetObject(hBmp, sizeof(bm), &bm);
+
+		BITMAPINFO RGB32BitsBITMAPINFO;
+		ZeroMemory(&RGB32BitsBITMAPINFO, sizeof(BITMAPINFO));
+		RGB32BitsBITMAPINFO.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+		RGB32BitsBITMAPINFO.bmiHeader.biWidth = bm.bmWidth;
+		RGB32BitsBITMAPINFO.bmiHeader.biHeight = bm.bmHeight;
+		RGB32BitsBITMAPINFO.bmiHeader.biPlanes = 1;
+		RGB32BitsBITMAPINFO.bmiHeader.biBitCount = 32;
+
+		UINT* ptPixels;
+
+		HBITMAP DirectBitmap = CreateDIBSection(DirectDC,
+			(BITMAPINFO*)&RGB32BitsBITMAPINFO,
+			DIB_RGB_COLORS,
+			(void**)&ptPixels,
+			NULL, 0);
+		if (DirectBitmap)
 		{
-			HBITMAP hTmpBitmap = (HBITMAP)NULL;
-			if (hBmpDC)
-				if (hBmp == (HBITMAP)GetCurrentObject(hBmpDC, OBJ_BITMAP))
-				{
-					hTmpBitmap = CreateBitmap(1, 1, 1, 1, NULL);
-					SelectObject(hBmpDC, hTmpBitmap);
-				}
+			HGDIOBJ PreviousObject = SelectObject(DirectDC, DirectBitmap);
+			BitBlt(DirectDC, 0, 0, bm.bmWidth, bm.bmHeight, hBmpDC, 0, 0, SRCCOPY);
 
-			HGDIOBJ PreviousBufferObject = SelectObject(BufferDC, hBmp);
-			// here BufferDC contains the bitmap
+			cOldColor = COLORREF2RGB(cOldColor);
+			cNewColor = COLORREF2RGB(cNewColor);
 
-			HDC DirectDC = CreateCompatibleDC(NULL); // DC for working
-			if (DirectDC)
+			for (int i = ((bm.bmWidth * bm.bmHeight) - 1); i >= 0; i--)
 			{
-				// Get bitmap size
-				BITMAP bm;
-				GetObject(hBmp, sizeof(bm), &bm);
-
-				// create a BITMAPINFO with minimal initilisation 
-				// for the CreateDIBSection
-				BITMAPINFO RGB32BitsBITMAPINFO;
-				ZeroMemory(&RGB32BitsBITMAPINFO, sizeof(BITMAPINFO));
-				RGB32BitsBITMAPINFO.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-				RGB32BitsBITMAPINFO.bmiHeader.biWidth = bm.bmWidth;
-				RGB32BitsBITMAPINFO.bmiHeader.biHeight = bm.bmHeight;
-				RGB32BitsBITMAPINFO.bmiHeader.biPlanes = 1;
-				RGB32BitsBITMAPINFO.bmiHeader.biBitCount = 32;
-
-				// pointer used for direct Bitmap pixels access
-				UINT* ptPixels;
-
-				HBITMAP DirectBitmap = CreateDIBSection(DirectDC,
-					(BITMAPINFO*)&RGB32BitsBITMAPINFO,
-					DIB_RGB_COLORS,
-					(void**)&ptPixels,
-					NULL, 0);
-				if (DirectBitmap)
-				{
-					// here DirectBitmap!=NULL so ptPixels!=NULL no need to test
-					HGDIOBJ PreviousObject = SelectObject(DirectDC, DirectBitmap);
-					BitBlt(DirectDC, 0, 0,
-						bm.bmWidth, bm.bmHeight,
-						BufferDC, lpRect->GetLeft(), lpRect->GetTop(), SRCCOPY);
-
-					// here the DirectDC contains the bitmap
-
-				 // Convert COLORREF to RGB (Invert RED and BLUE)
-					cOldColor = COLORREF2RGB(cOldColor);
-					cNewColor = COLORREF2RGB(cNewColor);
-
-					// After all the inits we can do the job : Replace Color
-					for (int i = ((bm.bmWidth * bm.bmHeight) - 1); i >= 0; i--)
-					{
-						if (ptPixels[i]) ptPixels[i] = cNewColor;
-					}
-					// little clean up
-					// Don't delete the result of SelectObject because it's 
-					// our modified bitmap (DirectBitmap)
-					SelectObject(DirectDC, PreviousObject);
-
-					// finish
-					RetBmp = DirectBitmap;
-				}
-				// clean up
-				DeleteDC(DirectDC);
+				cOldColor = COLORREF2RGB(ptPixels[i]);
+				if (ptPixels[i]) ptPixels[i] = cNewColor;
 			}
-			if (hTmpBitmap)
-			{
-				SelectObject(hBmpDC, hBmp);
-				DeleteObject(hTmpBitmap);
-			}
-			SelectObject(BufferDC, PreviousBufferObject);
-			// BufferDC is now useless
-			DeleteDC(BufferDC);
+
+			SelectObject(DirectDC, PreviousObject);
+
+			// finish
+			RetBmp = DirectBitmap;
 		}
+		// clean up
+		DeleteDC(DirectDC);
+	}
+	if (hTmpBitmap)
+	{
+		SelectObject(hBmpDC, hBmp);
+		DeleteObject(hTmpBitmap);
 	}
 	return RetBmp;
 }
 
 HBRUSH hFgBrush = NULL;
 HBRUSH hBkBrush = NULL;
-//WINGDIAPI BOOL  WINAPI RoundRect(_In_ HDC hdc, _In_ int left, _In_ int top, _In_ int right, _In_ int bottom, _In_ int width, _In_ int height);
-//typedef int(__stdcall* pfnRoundRect)(HDC hdc, int left, int top, int right, int bottom, int width, int height);
-//pfnRoundRect fpRoundRect = NULL;
-//int __stdcall MyRoundRect(HDC hdc, int left, int top, int right, int bottom, int width, int height)
-//{
-//	::SelectObject(hdc, hBkBrush);
-//	int rtn = fpRoundRect(hdc, left, top, right, bottom, width, height);
-//	return rtn;
-//}
 //WINGDIAPI HBRUSH  WINAPI CreateSolidBrush( _In_ COLORREF color);
 typedef HBRUSH(__stdcall* pfnCreateSolidBrush)(COLORREF color);
 pfnCreateSolidBrush fpCreateSolidBrush = NULL;
+__declspec(noinline)
 HBRUSH __stdcall MyCreateSolidBrush(COLORREF color)
 {
 	
@@ -149,6 +100,7 @@ HBRUSH __stdcall MyCreateSolidBrush(COLORREF color)
 //WINGDIAPI HPEN    WINAPI CreatePen(int iStyle, int cWidth, COLORREF color);
 typedef HPEN(__stdcall* pfnCreatePen)(int iStyle, int cWidth, COLORREF color);
 pfnCreatePen fpCreatePen = NULL;
+__declspec(noinline)
 HPEN __stdcall MyCreatePen(int iStyle, int cWidth, COLORREF color)
 {
 	HPEN rtn = fpCreatePen(iStyle, cWidth, FOREGOUND_COLOR);
@@ -158,6 +110,7 @@ HPEN __stdcall MyCreatePen(int iStyle, int cWidth, COLORREF color)
 
 typedef int(__stdcall* pfnFillRect)(HDC hdc, RECT* lprc, HBRUSH hbr);
 pfnFillRect fpFillRect = NULL;
+__declspec(noinline)
 int __stdcall MyFillRect(HDC hdc, RECT* lprc, HBRUSH hbr)
 {
 	::SelectObject(hdc, hBkBrush);
@@ -176,24 +129,28 @@ BOOL __stdcall MyFillRgn(HDC hdc, HRGN hrgn, HBRUSH hbr)
 //  WINGDIAPI HGDIOBJ WINAPI SelectObject(_In_ HDC hdc, _In_ HGDIOBJ h);
 typedef HGDIOBJ(__stdcall* pfnSelectObject)(HDC hdc, HGDIOBJ h);
 pfnSelectObject fpSelectObject = NULL;
+__declspec(noinline)
 HGDIOBJ __stdcall MySelectObject(HDC hdc, HGDIOBJ h)
 {
 	return fpSelectObject(hdc, h);
 }
 typedef COLORREF (__stdcall* pfnSetBkColor)(HDC hdc, COLORREF color);
 pfnSetBkColor fpSetBkColor = NULL;
+__declspec(noinline)
 COLORREF __stdcall MySetBkColor(HDC hdc, COLORREF color)
 {
 	return fpSetBkColor(hdc, BACKGOUND_COLOR);
 }
 typedef COLORREF (__stdcall* pfnSetBkMode)(HDC hdc, int mode);
 pfnSetBkMode fpSetBkMode = NULL;
+__declspec(noinline)
 COLORREF __stdcall MySetBkMode(HDC hdc, int mode)
 {
 	return fpSetBkMode(hdc, mode);
 }
 typedef COLORREF (__stdcall* pfnSetTextColor)(HDC hdc, COLORREF color);
 pfnSetTextColor fpSetTextColor = NULL;
+__declspec(noinline)
 COLORREF __stdcall MySetTextColor(HDC hdc, COLORREF color)
 {
 	return fpSetTextColor(hdc, FOREGOUND_COLOR);
@@ -201,34 +158,34 @@ COLORREF __stdcall MySetTextColor(HDC hdc, COLORREF color)
 typedef BOOL(__stdcall* pfnAlphaBlend)(HDC hdcDest, int xoriginDest, int yoriginDest, int wDest, int hDest,
 	HDC hdcSrc, int xoriginSrc, int yoriginSrc, int wSrc, int hSrc, BLENDFUNCTION ftn);
 pfnAlphaBlend fpAlphaBlend = NULL;
+__declspec(noinline)
 BOOL __stdcall MyAlphaBlend(HDC hdcDest, int xoriginDest, int yoriginDest, int wDest, int hDest,
 	HDC hdcSrc, int xoriginSrc, int yoriginSrc, int wSrc, int hSrc, BLENDFUNCTION ftn)
 {
 	//::SelectObject(hdcSrc, hBkBrush);
 	//::SelectObject(hdcDest, hBkBrush);
 	
-	Rect srcRect{ xoriginSrc, yoriginSrc, xoriginSrc + wSrc, yoriginSrc + hSrc };
+	Rect srcRect{ xoriginSrc, yoriginSrc, wSrc, hSrc };
 	HBITMAP hBmp = reinterpret_cast<HBITMAP>(GetCurrentObject(hdcSrc, OBJ_BITMAP));
 	HBITMAP nBitmap = ReplaceColor(hBmp, 0, RGB(0, 0, 255), hdcSrc, &srcRect);
-	//for (int ecx = xoriginSrc; ecx < wSrc + xoriginSrc; ++ecx)
-	//{
-	//	for (int ecy = yoriginSrc; ecy < hSrc + yoriginSrc; ++ecy)
-	//	{
-	//		COLORREF pixCol = GetPixel(hdcSrc, ecx, ecy);
-	//		if (pixCol)
-	//		{
-	//			pixCol = SetPixel(hdcSrc, ecx, ecy, RGB(0, 0, 255));
-	//			//
-	//		}
-	//	}
-	//}
+	/*for (int ecx = xoriginSrc; ecx < wSrc + xoriginSrc; ++ecx)
+	{
+		for (int ecy = yoriginSrc; ecy < hSrc + yoriginSrc; ++ecy)
+		{
+			COLORREF pixCol = GetPixel(hdcSrc, ecx, ecy);
+			if (pixCol)
+			{
+				pixCol = SetPixel(hdcSrc, ecx, ecy, RGB(0, 0, 255));
+			}
+		}
+	}*/
 	SelectObject(hdcSrc, nBitmap);
 
 	BLENDFUNCTION bf = { 0 };
 	bf.BlendOp = AC_SRC_OVER;
 	bf.BlendFlags = 0;
 	bf.AlphaFormat = 0;
-	bf.SourceConstantAlpha = 192;
+	bf.SourceConstantAlpha = 127;
 	BOOL blRtn = fpAlphaBlend(hdcDest, xoriginDest, yoriginDest, wDest, hDest, hdcSrc, xoriginSrc, yoriginSrc, wSrc, hSrc, bf);
 	DeleteObject(nBitmap);
 	//RECT targetRect = { xoriginDest, yoriginDest, xoriginDest + wDest, yoriginDest + hDest };
@@ -237,6 +194,7 @@ BOOL __stdcall MyAlphaBlend(HDC hdcDest, int xoriginDest, int yoriginDest, int w
 }
 typedef HPEN(__stdcall* pfnCreatePenIndirect)(CONST LOGPEN* plpen);
 pfnCreatePenIndirect fpCreatePenIndirect = NULL;
+__declspec(noinline)
 HPEN __stdcall MyCreatePenIndirect(LOGPEN* plpen)
 {
 	plpen->lopnColor = FOREGOUND_COLOR;
@@ -245,6 +203,7 @@ HPEN __stdcall MyCreatePenIndirect(LOGPEN* plpen)
 //BOOL  WINAPI BitBlt( _In_ HDC hdc, _In_ int x, _In_ int y, _In_ int cx, _In_ int cy, _In_opt_ HDC hdcSrc, _In_ int x1, _In_ int y1, _In_ DWORD rop);
 typedef BOOL(__stdcall* pfnBitBlt)(HDC hdc, int x, int y, int cx, int cy, HDC hdcSrc, int x1, int y1, DWORD rop);
 pfnBitBlt fpBitBlt = NULL;
+__declspec(noinline)
 BOOL __stdcall MyBitBlt(HDC hdc, int x, int y, int cx, int cy, HDC hdcSrc, int x1, int y1, DWORD rop)
 {
 	//::SelectObject(hdc, hBkBrush);
@@ -256,6 +215,7 @@ BOOL __stdcall MyBitBlt(HDC hdc, int x, int y, int cx, int cy, HDC hdcSrc, int x
 // WINGDIAPI BOOL  WINAPI StretchBlt(_In_ HDC hdcDest, _In_ int xDest, _In_ int yDest, _In_ int wDest, _In_ int hDest, _In_opt_ HDC hdcSrc, _In_ int xSrc, _In_ int ySrc, _In_ int wSrc, _In_ int hSrc, _In_ DWORD rop);
 typedef BOOL(__stdcall* pfnStretchBlt)(HDC hdcDest, int xDest, int yDest, int wDest, int hDest, HDC hdcSrc, int xSrc, int ySrc, int wSrc, int hSrc, DWORD rop);
 pfnStretchBlt fpStretchBlt = NULL;
+__declspec(noinline)
 BOOL __stdcall MyStretchBlt(HDC hdcDest, int xDest, int yDest, int wDest, int hDest, HDC hdcSrc, int xSrc, int ySrc, int wSrc, int hSrc, DWORD rop)
 {
 	BOOL blRtn = fpStretchBlt(hdcDest, xDest, yDest, wDest, hDest, hdcSrc, xSrc, ySrc, wSrc, hSrc, rop);
@@ -266,15 +226,17 @@ BOOL __stdcall MyStretchBlt(HDC hdcDest, int xDest, int yDest, int wDest, int hD
 // WINGDIAPI BOOL  WINAPI ExtTextOutW(_In_ HDC hdc, _In_ int x, _In_ int y, _In_ UINT options, _In_opt_ CONST RECT* lprect, _In_reads_opt_(c) LPCWSTR lpString, _In_ UINT c, _In_reads_opt_(c) CONST INT* lpDx);
 typedef BOOL(__stdcall* pfnExtTextOutW)(HDC hdc, int x, int y, UINT options, RECT* lprect, LPCWSTR lpString, UINT c, INT* lpDx);
 pfnExtTextOutW fpExtTextOutW = NULL;
+__declspec(noinline)
 BOOL __stdcall MyExtTextOutW(HDC hdc, int x, int y, UINT options, RECT* lprect, LPCWSTR lpString, UINT c, INT* lpDx)
 {
-	::SelectObject(hdc, hFgBrush);
+	// ::SelectObject(hdc, hFgBrush);
 	BOOL blRtn = fpExtTextOutW(hdc, x, y, options, lprect, lpString, c, lpDx);
 	return blRtn;
 }
 // WINGDIAPI BOOL WINAPI Rectangle(_In_ HDC hdc, _In_ int left, _In_ int top, _In_ int right, _In_ int bottom);
 typedef BOOL(__stdcall* pfnRectangle)(HDC hdc, int left, int top, int right, int bottom);
 pfnRectangle fpRectangle = NULL;
+__declspec(noinline)
 BOOL __stdcall MyRectangle(HDC hdc, int left, int top, int right, int bottom)
 {
 	BOOL blRtn = fpRectangle(hdc, left, top, right, bottom);
@@ -285,6 +247,7 @@ BOOL __stdcall MyRectangle(HDC hdc, int left, int top, int right, int bottom)
 // WINGDIAPI BOOL  WINAPI RoundRect(_In_ HDC hdc, _In_ int left, _In_ int top, _In_ int right, _In_ int bottom, _In_ int width, _In_ int height);
 typedef BOOL(__stdcall* pfnRoundRect)(HDC hdc, int left, int top, int right, int bottom, int width, int height);
 pfnRoundRect fpRoundRect = NULL;
+__declspec(noinline)
 BOOL __stdcall MyRoundRect(HDC hdc, int left, int top, int right, int bottom, int width, int height)
 {
 	::SelectObject(hdc, hBkBrush);
@@ -295,6 +258,7 @@ BOOL __stdcall MyRoundRect(HDC hdc, int left, int top, int right, int bottom, in
 }
 typedef Gdiplus::Status(__stdcall* pfnDrawString)(Gdiplus::Graphics* graphic, WCHAR* string, INT length, Gdiplus::Font* font, Gdiplus::RectF& layoutRect, Gdiplus::StringFormat* stringFormat, Gdiplus::Brush* brush);
 pfnDrawString fpDrawString = NULL;
+__declspec(noinline)
 Gdiplus::Status __stdcall MyDrawString(Gdiplus::Graphics* graphic, WCHAR* string, INT length, Gdiplus::Font* font, Gdiplus::RectF& layoutRect, Gdiplus::StringFormat* stringFormat, Gdiplus::Brush* brush)
 {
 	SolidBrush sbbrush(Color(255, 0, 0, 255));
@@ -304,6 +268,7 @@ Gdiplus::Status __stdcall MyDrawString(Gdiplus::Graphics* graphic, WCHAR* string
 typedef int(__stdcall* pfnDrawTextW)(_In_ HDC hdc, _When_((format & DT_MODIFYSTRING), _At_((LPWSTR)lpchText, _Inout_grows_updates_bypassable_or_z_(cchText, 4)))
 	_When_((!(format & DT_MODIFYSTRING)), _In_bypassable_reads_or_z_(cchText)) LPCWSTR lpchText, int cchText, LPRECT lprc, UINT format);
 pfnDrawTextW fpDrawTextW = NULL;
+__declspec(noinline)
 int __stdcall MyDrawTextW(_In_ HDC hdc, _When_((format& DT_MODIFYSTRING), _At_((LPWSTR)lpchText, _Inout_grows_updates_bypassable_or_z_(cchText, 4)))
 	_When_((!(format& DT_MODIFYSTRING)), _In_bypassable_reads_or_z_(cchText)) LPCWSTR lpchText, int cchText, LPRECT lprc, UINT format)
 {
